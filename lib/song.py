e7c70e39004df171fc2ae54d0b13209624a0c57f
kwamebrowne <theeecryptoking@gmail.com>
2023-05-28 01:25:54 -0500

    need more practice with .get and indexes in dists and lists

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,54 @@
 class Song:
-    pass
+    # song, name, genre 
+
+    count = 0
+    names = []
+    artists = []
+    genres = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        self.add_song_to_count()
+        self.add_to_genres(genre)
+        self.add_to_artists(artist)
+        self.add_to_genre_count(genre)
+        self.add_to_artist_count(artist)
+
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1 
+
+    @classmethod
+    def add_to_genres(cls, genre):
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+
+    @classmethod
+    def add_to_artists(cls, artist):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+    
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        # need to find a way to add genre and count or find how many times genre is being entered
+        # Song.hist(genre)
+        # need to work more with get and indexes i lists and dists
+        if cls.genre_count.get(genre):
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
+            
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        if cls.artist_count.get(artist):
+            cls.artist_count[artist] += 1
+        else:
+            cls.artist_count[artist] = 1
+        
+
+# ninety_nine_problems = Song("99 Problems", "Jay-Z", "Rap")
+# print(Song.count , Song.genres)
